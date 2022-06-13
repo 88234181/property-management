8ce8c5a949a6d64d29affa7cc947b705b171bc21
Junjie Zhao <junjiezhao722@gmail.com>
2022-06-12 21:51:31 -0700

    Added valid check for user controller

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +16,12 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner email is mandatory")
+    @NotEmpty(message = "Owner email cannot be empty")
+    @Size(min = 1, max = 50, message = "Owner email should be between 1 to 50 characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
